a739c9c8ee87eea420082f0a51687b7004ecd0e1
Arihant Pal <69034460+Ari1009@users.noreply.github.com>
2023-04-09 20:45:57 +0530

    result_print

--- Solu/utils/ObjectDetectorOptions.py
+++ Solu/utils/ObjectDetectorOptions.py
@@ -12,7 +12,17 @@ from tflite_support import metadata
 Interpreter = tf.lite.Interpreter
 load_delegate = tf.lite.experimental.load_delegate
 
+class ObjectDetectorOptions(NamedTuple):
 
+
+  max_results: int = -1
+  """The maximum number of top-scored detection results to return."""
+
+  num_threads: int = 4
+  """The number of CPU threads to be used."""
+
+  score_threshold: float = 0.25
+  """The score threshold of detection results to return."""
 
 
 
@@ -59,14 +69,6 @@ class ObjectDetector:
       model_path: str,
       options: ObjectDetectorOptions = ObjectDetectorOptions()
   ) -> None:
-    """Initialize a TFLite object detection model.
-    Args:
-        model_path: Path to the TFLite model.
-        options: The config to initialize an object detector. (Optional)
-    Raises:
-        ValueError: If the TFLite model is invalid.
-        OSError: If the current OS isn't supported by EdgeTPU.
-    """
 
     # Load metadata from model.
     displayer = metadata.MetadataDisplayer.with_model_file(model_path)
@@ -89,32 +91,14 @@ class ObjectDetector:
 #     label_list = list(filter(lambda x: len(x) > 0, label_map_file.splitlines()))
     self._label_list = ['rust', 'scab']#label_list
 
-    # Initialize TFLite model.
-    if options.enable_edgetpu:
-      if edgetpu_lib_name() is None:
-        raise OSError("The current OS isn't supported by Coral EdgeTPU.")
-      interpreter = Interpreter(
-          model_path=model_path,
-          experimental_delegates=[load_delegate(edgetpu_lib_name())],
-          num_threads=options.num_threads)
-    else:
-      interpreter = Interpreter(
+   
+    interpreter = Interpreter(
           model_path=model_path, num_threads=options.num_threads)
 
     interpreter.allocate_tensors()
     input_detail = interpreter.get_input_details()[0]
 
-    # From TensorFlow 2.6, the order of the outputs become undefined.
-    # Therefore we need to sort the tensor indices of TFLite outputs and to know
-    # exactly the meaning of each output tensor. For example, if
-    # output indices are [601, 599, 598, 600], tensor names and indices aligned
-    # are:
-    #   - location: 598
-    #   - category: 599
-    #   - score: 600
-    #   - detection_count: 601
-    # because of the op's ports of TFLITE_DETECTION_POST_PROCESS
-    # (https://github.com/tensorflow/tensorflow/blob/a4fe268ea084e7d323133ed7b986e0ae259a2bc7/tensorflow/lite/kernels/detection_postprocess.cc#L47-L50).
+  
     sorted_output_indices = sorted(
         [output['index'] for output in interpreter.get_output_details()])
     self._output_indices = {
@@ -130,14 +114,7 @@ class ObjectDetector:
     self._options = options
 
   def detect(self, input_image: np.ndarray) -> List[Detection]:
-    """Run detection on an input image.
-    Args:
-        input_image: A [height, width, 3] RGB image. Note that height and width
-          can be anything since the image will be immediately resized according
-          to the needs of the model within this function.
-    Returns:
-        A Person instance.
-    """
+   
     image_height, image_width, _ = input_image.shape
 
     input_tensor = self._preprocess(input_image)
@@ -184,17 +161,7 @@ class ObjectDetector:
   def _postprocess(self, boxes: np.ndarray, classes: np.ndarray,
                    scores: np.ndarray, count: int, image_width: int,
                    image_height: int) -> List[Detection]:
-    """Post-process the output of TFLite model into a list of Detection objects.
-    Args:
-        boxes: Bounding boxes of detected objects from the TFLite model.
-        classes: Class index of the detected objects from the TFLite model.
-        scores: Confidence scores of the detected objects from the TFLite model.
-        count: Number of detected objects from the TFLite model.
-        image_width: Width of the input image.
-        image_height: Height of the input image.
-    Returns:
-        A list of Detection objects detected by the TFLite model.
-    """
+
     results = []
 
     # Parse the model output into a list of Detection entities.
@@ -254,13 +221,7 @@ def visualize(
     image: np.ndarray,
     detections: List[Detection],
 ) -> np.ndarray:
-  """Draws bounding boxes on the input image and return it.
-  Args:
-    image: The input RGB image.
-    detections: The list of all "Detection" entities to be visualize.
-  Returns:
-    Image with bounding boxes.
-  """
+  
   for detection in detections:
     # Draw bounding_box
     start_point = detection.bounding_box.left, detection.bounding_box.top
